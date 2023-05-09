0e8c673f596ed1cebe9dc206c76e003d82bcd823
Hamigp <129912566+Hamigp@users.noreply.github.com>
2023-05-09 19:52:15 -0300

    imagen de carrito por icono

--- src/components/CardWidget.jsx
+++ src/components/CardWidget.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Carrito from "../Assets/img/Carrito.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faCartShopping} from "@fortawesome/free-solid-svg-icons"
 
