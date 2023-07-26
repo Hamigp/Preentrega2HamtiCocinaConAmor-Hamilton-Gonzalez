3672a127d23c6af584204ef968d9e1df220e7b8b
Hamigp <129912566+Hamigp@users.noreply.github.com>
2023-07-26 18:45:36 -0300

    correcion visual del item

--- src/Container/Item.jsx
+++ src/Container/Item.jsx
@@ -1,28 +1,37 @@
+import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
 
-
-const item = ({ item }) => {
+const Item = ({ item }) => {
   return (
-    <div style={{border:100}}>
-    <Card
-      style={{ width: "18rem", height: "600px" }}
-      className="float-start d-flex flex-column justify-content-between"
-    >
-      <Card.Text>
-        <b>{item.title}</b>
-      </Card.Text>
-      <Card.Img variant="top" src={item.pictureUrl} className="img-thumbnail" />
-      <Card.Body>
-        <Card.Title>{item.title}</Card.Title>
-        <Card.Text>{"$" + item.price} </Card.Text>
-        <Button variant="dark">
-          <Link style={{"textDecoration":"none", "color":"white"}} to={`/item/${item.id}`}>ver detalles</Link>
-        </Button>
-      </Card.Body>
-    </Card>
+    <div style={{ margin: "1rem" }}>
+      <Card
+        style={{ width: "18rem", height: "600px" }}
+        className="float-start d-flex flex-column justify-content-between"
+      >
+        <Card.Img
+          variant="top"
+          src={item.pictureUrl}
+          className="img-thumbnail"
+          style={{ width: "100%", height: "300px", objectFit: "cover" }}
+        />
+        <Card.Body>
+        <Card.Text>
+          <b>{item.title}</b>
+        </Card.Text>
+          <Card.Text>
+            <strong>{"$" + item.price}</strong>
+          </Card.Text>
+          <Button variant="dark">
+            <Link style={{ textDecoration: "none", color: "white" }} to={`/item/${item.id}`}>
+              Ver detalles
+            </Link>
+          </Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
-export default item;
+
+export default Item;
